17e888457b4ce753c1f5a7d4f921d77d54487fb8
xuanphong01bn <donhatphong94@gmail.com>
2022-08-12 01:39:16 +0700

    lay data tu db ra views

--- Server/src/config/ConnectDB.js
+++ Server/src/config/ConnectDB.js
@@ -4,7 +4,8 @@ const { Sequelize } = require('sequelize');
 // Option 3: Passing parameters separately (other dialects)
 const sequelize = new Sequelize('users', 'root', null, {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging: false
 });
 
 let testConnectDB = async () => {
--- Server/src/controllers/homeController.js
+++ Server/src/controllers/homeController.js
@@ -1,6 +1,19 @@
 import express from "express"
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs') // ko cần đường dẫn do trong file view engine đã config r mạc định là trong file views r
+import db from '../models/index'
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        // console.log('---------------------');
+        // console.log(data);
+        // console.log('---------------------');
+
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        }); // ko cần đường dẫn do trong file view engine đã config r mạc định là trong file views r
+
+    } catch (e) {
+        console.log(e);
+    }
 
 }
 module.exports = {
